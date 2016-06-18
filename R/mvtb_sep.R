89559dc3d614ae245e25094572d464e05f77de33
patr1ckm <patrick.mil10@gmail.com>
2016-06-18 12:32:48 -0400

    initial mvtb sep tests

---
+++ R/mvtb_sep.R
@@ -0,0 +1,100 @@
+#' Separate tuning of multivariate tree boosting models. 
+#' @export
+mvtb.sep <- function(Y,X,n.trees=100,
+                 shrinkage=.01,
+                 interaction.depth=1,
+                 distribution="gaussian",
+                 train.fraction=1,
+                 bag.fraction=1,
+                 cv.folds=1,
+                 subset=NULL,
+                 seednum=NULL,
+                 compress=FALSE,
+                 mc.cores=1,
+                 save.cv=FALSE,
+                 iter.details=TRUE,
+                 verbose=FALSE, ...) {
+  
+  Y <- as.data.frame(Y)
+  X <- as.data.frame(X)
+  n <- nTrain <- nrow(X)
+  
+  if(!is.null(seednum)) set.seed(seednum)
+  if(train.fraction < 1) nTrain <- ceiling(train.fraction*n) 
+  
+  if(is.null(subset)) { 
+    s <- 1:n 
+  } else {
+    nTrain <- NULL
+    s <- subset
+  }
+
+  do.one <- function(y, x, s, ...){ gbm.fit(y=y[s], x=x[s,], ...) }
+  
+  get.pred.err <- function(o, y, x, n.trees){
+    yhat.iter <- predict(o, newdata=x, n.trees=1:n.trees) 
+    apply(yhat.iter, 2, function(yhat, y){
+      var(y - yhat)
+    }, y=y)
+  }
+  get.test.err <- function(oL, Y, x, s, n.trees){
+    test.err <- vector(mode = "list", length=ncol(Y))
+    names(test.err) <- colnames(Y)
+    for(i in 1:ncol(Y)){
+      test.err[[i]] <- get.pred.err(o=oL[[i]], y=Y[-s, i], x=x[-s, ], n.trees=n.trees)
+    }
+    return(test.err)
+  }
+  
+  cv.mods <- cv.mod.err <- vector(mode="list", length(ncol(Y)))
+  
+  if(cv.folds > 1){
+    folds <- sample(1:cv.folds, size=length(s), replace=T)
+    for(i in 1:cv.folds){
+      train <- s[folds != i]
+      test <- s[folds == i]
+      cv.mods[[i]] <- mclapply(Y, FUN=do.one, x=X, n.trees=n.trees, shrinkage=shrinkage, 
+                             interaction.depth=interaction.depth, distribution=distribution,
+                             nTrain=nTrain, bag.fraction=bag.fraction, s=s, 
+                             keep.data=FALSE, verbose=verbose, mc.cores=mc.cores,...)
+      cv.mod.err[[i]] <- get.test.err(mods, Y=Y, x=X, n.trees=n.trees, s=test)
+    }
+  }
+  
+  mods <- mclapply(Y, FUN=do.one, x=X, n.trees=n.trees, shrinkage=shrinkage, 
+                 interaction.depth=interaction.depth, distribution=distribution,
+                 nTrain=nTrain, bag.fraction=bag.fraction, s=s, 
+                 keep.data=FALSE, verbose=verbose, mc.cores=mc.cores,...)
+  
+  train.err <- lapply(mods, function(o){ o$train.error})
+  oob.err <- lapply(mods, function(o){ -cumsum(o$oobag.improve)})
+  if(cv.folds > 1) {
+    cv.err <- lapply(1:ncol(Y), function(i) { rowMeans(sapply(cv.mod.err, "[[", i)) })
+  } else {
+    cv.err <- lapply(1:ncol(Y), function(i){ rep(NaN, n.trees) })
+  }
+  names(cv.err) <- colnames(Y)
+  if(is.null(subset)){
+    test.err <- lapply(mods, function(o){ o$valid.error})
+  } else {
+    test.err <- get.test.err(mods, Y=Y, x=X, s=s, n.trees=n.trees)
+  }
+  
+  # which.min can return length 0, return NA instead
+  which.min.na <- function(x){
+    res <- which.min(x)
+    if(length(res) == 0){
+      res <- NA
+    }
+    res
+  }
+  
+  best.trees <- data.frame(train = sapply(train.err, which.min.na), 
+                     test = sapply(test.err, which.min.na),
+                     oob = sapply(oob.err, which.min.na), 
+                     cv = sapply(cv.err, which.min.na))
+  
+  return(list(mods=mods, best.trees=best.trees, 
+              train.err=train.err, oob.err=oob.err, cv.err=cv.err, test.err=test.err))
+   
+}
