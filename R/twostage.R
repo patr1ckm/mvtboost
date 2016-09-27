900b8370fbbf5ea798c9a07923d21771efe82a7d
patr1ckm <patrick.mil10@gmail.com>
2016-09-27 12:12:57 -0400

    influence method for twostage, works with package (rough, needs better name)

---
+++ R/twostage.R
@@ -0,0 +1,44 @@
+#' @export
+twostage <- function(y, x, id, gbm.first=T, ...){
+  x <- as.data.frame(x)
+  if(gbm.first){
+    d <- data.frame(y, x)
+    ob <- gbm::gbm(y ~ . , data=d, distribution="gaussian", ...)
+    tr <- suppressWarnings(gbm::gbm.perf(ob, plot.it=F))
+    res <- y - predict(ob, newdata = d, n.trees = tr)
+    df <- data.frame(y=res, x, id=id)
+    form <- as.formula(paste0("y ~ 1 + ", paste0(colnames(x), collapse = " + "),  " + (",
+                              paste0(colnames(x), collapse=" + "), " + 1 || id)"))
+    ol <- lme4::lmer(form, data=df)
+  } else {
+    df <- data.frame(y=res, x=x, id=id)
+    form <- as.formula(paste0("y ~ 1 + ", paste0(colnames(x), collapse = " + "),  " + (",
+                              paste0(colnames(x), collapse=" + "), " + 1 || id)"))
+    ol <- lme4::lmer(form, data=df)
+    res <- y - fitted(ol)
+    df <- data.frame(y=res, x=x, id=id)
+    ob <- gbm::gbm(y ~ . -id, data=d, distribution="gaussian", ...)
+    tr <- suppressWarnings(gbm::gbm.perf(ob, plot.it=F))
+  }
+  res <- list(o.lmer=ol, o.gbm=ob, tr=tr)
+  class(res) <-  "twostage"
+  return(res)
+}
+
+#' @export
+influence.twostage <- function(object, n.trees = NULL, sort = FALSE, relative = TRUE){
+  if(is.null(n.trees)){ n.trees = object$tr}
+  inf <- gbm::relative.influence(object$o.gbm, n.trees = n.trees, scale. = FALSE, sort. = sort)
+  if(relative){
+    inf <- inf / sum(inf) * 100
+  }
+  return(inf)
+}
+
+
+#' @export
+predict.twostage <- function(object, newdata){
+  yhat <- predict(object$o.lmer, newdata=newdata) + 
+    predict(object$o.gbm, newdata=newdata, n.trees=object$tr)
+  return(yhat)
+}
---
+++ tests/testthat/test_twostage.R
@@ -0,0 +1,22 @@
+
+
+set.seed(104)
+ngroups <- 100
+group_size <- 10
+n <- ngroups * group_size
+id <- factor(rep(1:ngroups, each = group_size))
+
+train <- sample(1:800, size = 500, replace = FALSE)
+
+x <- rnorm(n)
+Z <- model.matrix(~id + x:id - 1)
+u <- rnorm(ncol(Z), 0, 1)
+y <- x * .5 + Z %*% u + rnorm(n)
+X <- as.data.frame(x)
+
+o <- twostage(y=y, x=data.frame(xfull), id=id, n.trees=1000, shrinkage=.005)
+d <- data.frame(y, x, id)
+yhat <- predict(o, newdata=d)
+mse <- mean((yt - yhat)^2)
+
+ri <- influence(o)
