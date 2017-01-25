b085817b63af18b97c9d60d6fb2eddd089bcb96b
patr1ckm <patrick.mil10@gmail.com>
2017-01-25 15:37:15 -0500

    remove mvtb_sep from tests and change influence to importance

--- R/mvtb_influence.R
+++ R/mvtb_influence.R
@@ -1,16 +1,15 @@
-#' Computes the relative influence of each predictor for each outcome
+#' Computes the relative importance of each predictor for each outcome
 #' 
-#' The relative influence of a predictor is the reduction in sums of squares attributable to splits on individual predictors.
+#' The relative importance of a predictor is the reduction in sums of squares attributable to splits on individual predictors.
 #' It is often expressed as a percent (sums to 100).
 #' @param model \code{mvtb} output model
 #' @param n.trees number of trees to use. Defaults to the minimum number of trees by CV, test, or training error for each outcome.
-#' @param relative How to scale the multivariate influences. If \code{"col"}, each column sums to 100. If \code{"tot"}, the whole matrix sums to 100 (a percent). Otherwise, the raw reductions in SSE are returned.
-#' @param sort whether or not results should be (reverse) sortd. Defaults to FALSE.
-#' @param ... Additional arguments passed to \code{gbm::relative_influence}
-#' @return Matrix of (relative) influences.
+#' @param relative How to scale the multivariate importances. If \code{"col"}, each column sums to 100. If \code{"tot"}, the whole matrix sums to 100 (a percent). Otherwise, the raw reductions in SSE are returned.
+#' @param sort whether or not results should be (reverse) sorted. Defaults to FALSE.
+#' @param ... Additional arguments passed to \code{gbm::relative_importance}
+#' @return Matrix of (relative) importances.
 #' @export 
 mvtb.ri <- function(model, n.trees=NULL, relative="col", sort = FALSE, ...){
-  
   if(any(unlist(lapply(model,function(li){is.raw(li)})))){
     model <- mvtb.uncomp(model)
   }
@@ -21,7 +20,7 @@ mvtb.ri <- function(model, n.trees=NULL, relative="col", sort = FALSE, ...){
   ri <- matrix(0,nrow=length(model$xnames),ncol=k)
   for(i in 1:k) {
     gbm.obj <- model$models[[i]]
-    ri[,i] <- gbm::relative_influence(gbm.obj,num_trees=n.trees[i], sort_it=sort, ...)
+    ri[,i] <- gbm::relative_importance(gbm.obj,num_trees=n.trees[i], sort_it=sort, ...)
   }
   if(relative == "col"){
     ri <- matrix(apply(ri,2,function(col){col/sum(col)})*100, 
@@ -34,46 +33,54 @@ mvtb.ri <- function(model, n.trees=NULL, relative="col", sort = FALSE, ...){
   return(ri)  
 }
 
+#' Compute variable importance (influence) scores from \code{mvtboost} models.
+#' @param model object from \code{mvtb, lmerboost, pcb, twostage}
+#' @param n.trees number of trees to use. Defaults to the minimum number of trees by CV, test, or training error for each outcome.
+#' @param relative How to scale the multivariate importances. If \code{"col"}, each column sums to 100. If \code{"tot"}, the whole matrix sums to 100 (a percent). Otherwise, the raw reductions in SSE are returned.
+#' @param sort whether or not results should be (reverse) sorted. Defaults to FALSE.
+#' @param ... not used
+#' @export
+importance <- function(model, n.trees=NULL, relative=TRUE, sort=TRUE, ...){
+  UseMethod("importance")
+}
 
-#' @importFrom stats influence
-NULL
 
-
-#' Compute influence scores from mvtb
 #' @inheritParams mvtb.ri
 #' @export
-influence.mvtb <- mvtb.ri
+#' @describeIn importance mvtb
+importance.mvtb <- mvtb.ri
 
 #' @inheritParams mvtb.ri
 #' @export
-influence.twostage <- function(model, n.trees = NULL, relative = TRUE, sort = FALSE, ...){
+#' @describeIn importance twostage
+importance.twostage <- function(model, n.trees = NULL, relative = TRUE, sort = FALSE, ...){
   if(is.null(n.trees)){ n.trees = model$tr}
-  inf <- gbm::relative_influence(model$o.gbm, num_trees = n.trees, rescale = FALSE, sort_it = sort)
+  inf <- gbm::relative_importance(model$o.gbm, num_trees = n.trees, rescale = FALSE, sort_it = sort)
   if(relative){
     inf <- inf / sum(inf) * 100
   }
   return(inf)
 }
 
-#' Compute influence scores from lmerboost
 #' @inheritParams mvtb.ri
 #' @export
-influence.lmerboost <- function(model, n.trees = NULL, relative = TRUE, sort = FALSE, ...){
+#' @describeIn importance lmerboost
+importance.lmerboost <- function(model, n.trees = NULL, relative = TRUE, sort = FALSE, ...){
   if(is.null(n.trees)){ 
     n.trees <- min(model$best.trees, na.rm = TRUE)
   }
-  inf <- influence_from_tree_list(model$trees, n.trees = n.trees, 
+  inf <- importance_from_tree_list(model$trees, n.trees = n.trees, 
                                              var.names = model$xnames)
   if(relative) { inf <- (inf / sum(inf)) * 100 }
   if(sort) { inf <- sort(inf, decreasing = TRUE) }
   inf
 }
 
-#' Compute influence scores from pcb
 #' @inheritParams mvtb.ri
 #' @export
-influence.pcb <- function(model, n.trees = NULL, relative = "col", sort = FALSE, ...){
-  ri <- influence.mvtb(model = model, n.trees=n.trees, relative = FALSE, sort=sort) 
+#' @describeIn importance pcb
+importance.pcb <- function(model, n.trees = NULL, relative = "col", sort = FALSE, ...){
+  ri <- importance.mvtb(model = model, n.trees=n.trees, relative = FALSE, sort=sort) 
   ri <- data.frame(ri %*% t(model$ev$vectors))
   
   if(relative == "col"){
@@ -87,7 +94,7 @@ influence.pcb <- function(model, n.trees = NULL, relative = "col", sort = FALSE,
   return(ri)
 }
 
-influence_from_tree_list <- function(model, n.trees=1, var.names) {
+importance_from_tree_list <- function(model, n.trees=1, var.names) {
   get.rel.inf <- function(obj) {
     lapply(split(obj[[6]], obj[[1]]), sum)
   }
--- tests/testthat/test_mvtb_params.R
+++ tests/testthat/test_mvtb_params.R
@@ -14,15 +14,11 @@ test_that("n.trees", {
   r <- mvtb(X=X, Y=Y, n.trees=50)
   expect_equal(r$best.trees$train, rep(50, 4))
   
-  r <- mvtb_sep(X=X, Y=Y, n.trees=50)
-  expect_equal(r$best.trees$train, rep(50, 4))
 })
 
 test_that("train.fraction", {
 for(i in seq(.1, .9, by=.1)) {
     r <- mvtb(X=X, Y=Y, n.trees=1, train.fraction=i)
-    expect_equal(r$models[[1]]$params$num_train, floor(n*i))
-    r <- mvtb_sep(X=X, Y=Y, n.trees=1, train.fraction=i)
     expect_equal(r$models[[1]]$params$num_train, floor(n*i))
 }
 })
@@ -30,20 +26,16 @@ for(i in seq(.1, .9, by=.1)) {
 test_that("bag.fraction", {
   r <- mvtb(X=X, Y=Y, n.trees=10, train.fraction=1, bag.fraction=.5)
   for(i in 1:4) { expect_equal(r$models[[i]]$params$bag_fraction, .5) }
-  r <- mvtb_sep(X=X, Y=Y, n.trees=10, train.fraction=1, bag.fraction=.5)
-  for(i in 1:4) { expect_equal(r$models[[i]]$params$bag_fraction, .5) }
 })
 
 test_that("subsetting", {
   r <- mvtb(X=X,Y=Y,n.trees=5, train.fraction=.5,bag.fraction=.5,s=1:500,save.cv=TRUE)
   expect_equal(r$s, 1:500) # should be in this order correct order
-  r <- mvtb_sep(X=X,Y=Y,n.trees=5, train.fraction=.5,bag.fraction=.5,s=1:500,save.cv=TRUE)
-  expect_equal(r$s, 1:500) # should be in this order correct order
-  
+
 })
 
 test_that("seednum", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     r <- f(X=X,Y=Y,n.trees=5, train.fraction=.5, bag.fraction=.5,s=1:500,seednum=8)
     expect_equal(r$params$seednum,8)
     r2 <- f(X=X,Y=Y,n.trees=5, train.fraction=.5, bag.fraction=.5,s=1:500,seednum=8)
@@ -54,7 +46,7 @@ test_that("seednum", {
 })
 
 test_that("compress", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     r <- f(X=X,Y=Y,n.trees=5, train.fraction=.5, bag.fraction=.5,s=1:500,seednum=8, compress=FALSE)
     expect_true(all(!(sapply(r,class) %in% "raw"))) # none of the objects are raw
     
@@ -73,7 +65,7 @@ test_that("compress", {
 })
 
 test_that("mvtb.uncomp", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     rc <- f(X=X,Y=Y,n.trees=5, seednum=8, compress=TRUE)
     r  <- f(X=X,Y=Y,n.trees=5, seednum=8, compress=FALSE)
     r2 <- mvtb.uncomp(rc)
@@ -89,13 +81,7 @@ test_that("iter.details", {
     expect_length(r$train.err, r$best.trees$train[1])
     expect_length(r$cv.err, r$best.trees$train[1])
     
-    r <- mvtb_sep(X=X,Y=Y,n.trees=5, seednum=8, compress=FALSE, cv.folds=3, save.cv=T, iter.details = T)
-    expect_true(all(c("train.err", "test.err", "cv.err", "cv.mods") %in% names(r)))
-    expect_length(r$test.err[[1]], r$best.trees$train[1])
-    expect_length(r$train.err[[1]], r$best.trees$train[1])
-    expect_length(r$cv.err[[1]], r$best.trees$train[1])
-    
-    for(f in c(mvtb, mvtb_sep)){
+    for(f in c(mvtb)){
       r <- f(X=X,Y=Y,n.trees=5, seednum=8, compress=FALSE, cv.folds=3, save.cv=F, iter.details = F)
       expect_null(r$cv.mods)
       r <- f(X=X,Y=Y,n.trees=5, seednum=8, compress=FALSE, cv.folds=3, save.cv=T, iter.details = F)
@@ -105,7 +91,7 @@ test_that("iter.details", {
 })
 
 test_that("verbose", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     expect_silent(f(X=X,Y=Y,n.trees=5, train.fraction=.5, bag.fraction=.5,s=1:500,seednum=8, compress=FALSE, cv.folds=3, save.cv=T, verbose = F))
     expect_output(f(X=X,Y=Y,n.trees=5, train.fraction=.5, bag.fraction=.5,s=1:500,seednum=8, compress=FALSE, cv.folds=3, save.cv=T, verbose = T))
   }
@@ -116,7 +102,7 @@ test_that("verbose", {
 # r <- mvtb(X=X,Y=Y,n.trees=500, train.fraction=.5, bag.fraction=.5,s=1:500,seednum=8, compress=FALSE, cv.folds=3, save.cv=T, mc.cores=3)
 
 test_that("keep.data", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     r <- f(X=X,Y=Y,n.trees=50, keep.data=FALSE)
     expect_null(r$models[[1]]$gbm_data_obj)
     r <- f(X=X,Y=Y,n.trees=50, keep.data=TRUE)
@@ -125,7 +111,7 @@ test_that("keep.data", {
 })
 
 test_that("distribution", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     r <- f(X=X,Y=Y,n.trees=50)
     expect_equal(r$models[[1]]$distribution$name,"Gaussian") # default is gaussian
     r <- f(X=X,Y=Y,n.trees=50, distribution="Gaussian")   
@@ -136,7 +122,7 @@ test_that("distribution", {
 
 test_that("train.fraction and s", {
 ## Test 
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     for(i in seq(.1,.9,by=.1)) {    
         r <- f(X=X,Y=Y,n.trees=10,train.fraction=i,s=1:floor(n*i),save.cv=TRUE)
         expect_true(all(r$s %in% 1:floor(n*i)))
@@ -145,7 +131,7 @@ test_that("train.fraction and s", {
 })
 
 test_that("interaction depth", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     r <- f(X=X,Y=Y,n.trees=50, interaction.depth=1)
     r2 <- f(X=X,Y=Y,n.trees=50, interaction.depth=2)
     r3 <- f(X=X,Y=Y,n.trees=50, interaction.depth=2)
@@ -164,7 +150,7 @@ test_that("interaction depth", {
 })
 
 test_that("checks", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     # test errors
     expect_error(f(X=X,Y=Y,n.trees=50, shrinkage=2))
     expect_error(f(X=X,Y=Y,n.trees=50, shrinkage=0))
@@ -181,7 +167,7 @@ test_that("checks", {
 })
 
 test_that("input", {
-  for(f in c(mvtb, mvtb_sep)){
+  for(f in c(mvtb)){
     # test data frame
     Y <- X %*% B + E
     Xf <- as.data.frame(X)
