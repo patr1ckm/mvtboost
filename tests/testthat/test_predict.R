969d5ec9c2ab7b960f6959cc028484b3ddd6e0f9
patr1ckm <patrick.mil10@gmail.com>
2017-02-10 16:43:50 -0500

    remove mvtb_sep from test_predict.R

--- tests/testthat/test_predict.R
+++ tests/testthat/test_predict.R
@@ -18,20 +18,15 @@ d1 <- data.frame(y=Y[,1],X1=X[,1],X2=X2)
 
 m <- mvtb(X=Xf,Y=Y,n.trees=50,interaction.depth=3,shrinkage=.5,bag.fraction=1,train.fraction=1,compress=FALSE,cv.folds=1,s=1:1000)
 m1 <- gbm::gbm(y~.,distribution="gaussian",n.trees=50,interaction.depth=3,data=d1,bag.fraction=1,train.fraction=1,shrinkage=.5)
-m2 <- mvtb_sep(Y=Y, X=Xf, n.trees=50, interaction.depth=3, bag.fraction=1, compress=F, cv.folds=1, s=1:1000, shrinkage=.5)
 
 test_that("predictions - mixed continuous categorical", {
   expect_equal(m$models[[1]]$c.splits,m1$c.splits)
-  expect_equal(m2$models[[1]]$c.splits,m1$c.splits)
   expect_equal(m1$initF,m$models[[1]]$initF)
-  expect_equal(m1$initF,m2$models[[1]]$initF)
   
   for(i in c(1:10,20,50)) {
        p <- predict(m,newdata=Xf,n.trees=i) 
        p1 <- predict(m1,newdata=Xf,n.trees=i)
-       p2 <- predict(m2,newdata=Xf,n.trees=i)
        expect_equal(c(p),p1,info=paste0("ntrees = ",i))
-       expect_equal(c(p2),p1,info=paste0("ntrees = ",i))
   }
 })
 
@@ -51,15 +46,12 @@ d1 <- data.frame(y=Y[,1],X)
 
 m <- mvtb(X=X,Y=Y,n.trees=50,shrinkage=.5,bag.fraction=1,train.fraction=1,compress=FALSE,cv.folds=1,s=1:1000)
 m1 <- gbm::gbm(y~.,distribution="gaussian",n.trees=50,data=d1,bag.fraction=1,train.fraction=1,shrinkage=.5)
-m2 <- mvtb_sep(X=X,Y=Y,n.trees=50,shrinkage=.5,bag.fraction=1,train.fraction=1,compress=FALSE,cv.folds=1,s=1:1000)
 
 test_that("predictions - categorical", {
   for(i in c(1,2,3,10,50)) {
       p <- predict(m,newdata=newdata,n.trees=i) 
       p1 <- predict(m1,newdata=newdata,n.trees=i)
-      p2 <- predict(m2,newdata=newdata,n.trees=i) 
       expect_equal(p,p1,info=paste0("ntrees = ",i))
-      expect_equal(p2,p1,info=paste0("ntrees = ",i))
   }
 })
 
@@ -80,15 +72,12 @@ d2 <- data.frame(y=Y[,1],Xf)
 set.seed(100)
 m <- mvtb(X=Xf,Y=Y,n.trees=50,shrinkage=.5,interaction.depth=3,bag.fraction=1,train.fraction=1,compress=FALSE,cv.folds=1,s=1:1000)
 m1 <- gbm::gbm(y~.,distribution="gaussian",n.trees=50,data=d2,interaction.depth=3,bag.fraction=1,train.fraction=1,shrinkage=.5)
-m2 <- mvtb_sep(X=Xf,Y=Y,n.trees=50,shrinkage=.5,interaction.depth=3,bag.fraction=1,train.fraction=1,compress=FALSE,cv.folds=1,s=1:1000)
 
 test_that("two continuous", {
   for(i in c(1:10,20,50)) {
       p <- predict(m,newdata=newdata,n.trees=i) 
       p1 <- predict(m1,newdata=newdata,n.trees=i)
-      p2 <- predict(m2,newdata=newdata,n.trees=i)
       expect_equal(c(p),p1,info=paste0("ntrees = ",i))
-      expect_equal(c(p2),p1,info=paste0("ntrees = ",i))
   }
 })
 
